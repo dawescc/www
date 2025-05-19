b407a9583657c1ae2689a7c1251fc87195f13439
Ryan Dawes <dev@dawes.cc>
2025-05-19 13:37:56 -0400

    init
    added project files
    added github files
    
    fixed content shift
    
    corrected nav buttons overflow

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+console.log("Hello!\nContact me: hello@dawes.cc");
+
+console.log(
+	`%c ________________________________________
+< mooooooooooooooooooooooooooooooooooooo >
+ ----------------------------------------
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`,
+	"font-family:monospace"
+);
