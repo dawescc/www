1efc588fe4b2c8b7127360051ccf7e34c72c80e5
Ryan Dawes <dev@dawes.cc>
2025-07-29 22:01:28 -0400

    v0.1.6
    Improved look and feel of tiles, reading lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "@fontsource-variable/geist";
 import "@fontsource-variable/merriweather-sans/wght-italic.css";
+import "@fontsource-variable/literata/wght-italic.css";
 import "./index.css";
 import RunYTD from "./partials/_ytd-run";
 import Book from "./partials/_book";
@@ -20,7 +21,7 @@ export function App() {
 							I'm <span className='selected'>Ryan</span>, a design engineer delivering high quality content products to massive audiences.
 						</h2>
 					</div>
-					<div className='flex flex-wrap gap-4'>
+					<div className='flex flex-col sm:flex-row gap-4'>
 						<div className='flex-1 tile'>
 							<p>
 								I have <span className='selected'>run</span> over <RunYTD />
@@ -28,7 +29,10 @@ export function App() {
 							</p>
 						</div>
 
-						<div className='flex-1 tile overflow-hidden relative group'>
+						<a
+							href='https://github.com/dawescc'
+							title='github/dawescc'
+							className='clean flex-1 tile overflow-hidden relative group'>
 							<p className='relative z-1'>
 								You can find some of my personal work on{" "}
 								<a
@@ -41,13 +45,16 @@ export function App() {
 							<div className='absolute h-auto w-full -rotate-6 right-0 bottom-0 translate-20 dark:invert opacity-5 select-none group-hover:opacity-10 transition-opacity ease-in'>
 								<img src='https://cdn-icons-png.flaticon.com/256/25/25231.png' />
 							</div>
-						</div>
+						</a>
 					</div>
 
 					<div className='tile bg-transparent bg-gradient-to-b from-layer-1 to-transparent to-90% flex flex-col gap-10'>
 						<p>
 							Here are some <span className='selected'>books</span> I have on my list.
 						</p>
+					</div>
+
+					<div className='-mx-2 grid grid-cols-1 gap-10'>
 						{booksData.items.map((readList) => (
 							<div
 								key={readList.key}
@@ -72,7 +79,7 @@ export function App() {
 				</section>
 				<img
 					src={logo}
-					className='size-6'
+					className='size-6 mt-10'
 				/>
 			</main>
 		</div>
--- src/partials/_book.tsx
+++ src/partials/_book.tsx
@@ -11,20 +11,34 @@ interface BookProps {
 
 export default function Book({ book }: BookProps) {
 	return (
-		<div className='w-[75%] md:w-[450px] h-[200px] flex-none p-5 flex gap-5 rounded-3xl bg-layer-2 hover:scale-95 transition-[scale] ease-in-out items-center'>
-			<div className='shrink-0 bg-layer-2 w-[108px] h-[162px] overflow-clip rounded-md'>
+		<div className='w-[350px] md:w-[450px] h-[200px] flex-none p-5 flex gap-5 rounded-3xl bg-layer-2 hover:scale-95 transition-[scale] ease-in-out items-center relative overflow-clip'>
+			{book.coverUrl && (
+				<div className='absolute z-0 inset-0 w-full h-auto -translate-y-1/2 scale-150 opacity-20 blur-lg select-none'>
+					<img
+						src={book.coverUrl}
+						alt={`Cover for ${book.title}`}
+						height={162}
+						width={108}
+						className='aspect-auto w-full h-auto object-cover'
+					/>
+				</div>
+			)}
+			<div className='relative z-1 shrink-0 bg-layer-2 overflow-clip rounded-md'>
 				{book.coverUrl && (
 					<img
 						src={book.coverUrl}
 						alt={`Cover for ${book.title}`}
-						className='w-full h-full object-cover'
+						height={162}
+						width={108}
+						className='aspect-auto w-full h-auto object-cover'
 					/>
 				)}
 			</div>
-			<div className='py-5 h-full flex flex-col justify-between text-2xl font-bold'>
+			<div className='z-1 py-5 h-full flex flex-col justify-between text-2xl font-black w-full'>
 				<a
 					href={book.bookLink}
-					className='font-serif leading-7 clean hover:underline line-clamp-3'>
+					title={book.title}
+					className='font-liberata leading-7 clean hover:underline line-clamp-3 break-words hyphens-auto backdrop-blur-sm opacity-85 dark:opacity-90'>
 					{book.title}
 				</a>
 				<p className='truncate text-sm text-accent'>{book.authors.join(", ") || "N/A"}</p>
